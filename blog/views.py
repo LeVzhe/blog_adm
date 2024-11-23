f1dfb04fadf0f776b66a51be3d9b101e430a860a
LeVzhe <levzh93@gmail.com>
2024-11-23 15:05:02 +0300

    feat: complete first template html code for list and details

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ def post_list(request):
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request):
+def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
 
     return render(request, "blog/post/detail.html", {"post": post})
