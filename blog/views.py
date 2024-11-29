6cd4c38e3d37af245e415394b3448ba9c0eb2f3a
LeVzhe <levzh93@gmail.com>
2024-11-29 21:45:17 +0300

    feat: add tags logik

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 from .forms import CommentForm, EmailPostForm
 from .models import Post
@@ -38,8 +39,12 @@ def post_share(request, post_id):
     )
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page", 1)
     try:
@@ -48,7 +53,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
 # class PostListView(ListView):
