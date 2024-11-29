975c42b6656f3a16dbf8751d36b8e87722c02ca3
LeVzhe <levzh93@gmail.com>
2024-11-29 21:28:44 +0300

    feat: change class to def

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# from django.views.generic import ListView
 from django.conf import settings
 
 from .models import Post, Comment
@@ -39,28 +40,28 @@ def post_share(request, post_id):
     )
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get("page", 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, "blog/post/list.html", {"posts": posts})
+def post_list(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
-class PostListView(ListView):
-    """
-    Алтернативное представление списка постов на основе классов (вместо post_list())
-    """
+# class PostListView(ListView):
+#     """
+#     Алтернативное представление списка постов на основе классов (вместо post_list())
+#     """
 
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "blog/post/list.html"
+#     queryset = Post.published.all()
+#     context_object_name = "posts"
+#     paginate_by = 3
+#     template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
