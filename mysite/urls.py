95647b15b38678eaa8e129bbb071ddfb9b38d90b
LeVzhe <levzh93@gmail.com>
2024-12-02 21:27:00 +0300

    fix: fix lines by isort

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.sitemaps.views import sitemap
+from django.urls import include, path
+
 from blog.sitemaps import PostSitemap
 
 sitemaps = {
