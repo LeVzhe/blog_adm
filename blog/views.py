246e88a3308d08dec482ac2b9c54416cb2fc73f1
LeVzhe <levzh93@gmail.com>
2024-12-03 21:42:55 +0300

    feat: create form and view to search logic

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 # from django.views.generic import ListView
+from re import search
 from django.conf import settings
+from django.contrib.postgres.search import SearchVector
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -7,7 +9,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 
 
@@ -111,3 +113,23 @@ def post_comment(request, post_id):
         "comment": comment,
     }
     return render(request, "blog/post/comment.html", content)
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
+    )
