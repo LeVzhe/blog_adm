e9ee77a31f219e5304119084c95997565859ad96
LeVzhe <levzh93@gmail.com>
2024-12-03 22:17:07 +0300

    fix: change search technologies (stay trigram)

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.contrib.postgres.search import TrigramSimilarity
 
 from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
@@ -123,16 +124,28 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            search_vector = SearchVector("title", weight="A") + SearchVector(
-                "body", weight="B"
-            )
-            search_query = SearchQuery(query)
+
+            #### Блок кода показательный. Для обычного поиска с приоритетом ####
+            # search_vector = SearchVector("title", weight="A") + SearchVector(
+            #     "body", weight="B"
+            # )
+            # search_query = SearchQuery(query)
+            # results = (
+            #     Post.published.annotate(
+            #         search=search_vector, rank=SearchRank(search_vector, search_query)
+            #     )
+            #     .filter(rank__gte=0.3)
+            #     .order_by("-rank")
+            # )
+            #####
+
+            #### Применение триграммного поиска ####
             results = (
                 Post.published.annotate(
-                    search=search_vector, rank=SearchRank(search_vector, search_query)
+                    similarity=TrigramSimilarity("title", query),
                 )
-                .filter(rank__gte=0.3)
-                .order_by("-rank")
+                .filter(similarity__gt=0.1)
+                .order_by("-similarity")
             )
 
     return render(
